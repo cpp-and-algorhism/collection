9982245243aab734320210d8cfc110700a6fb70a
ymjacky <jacky.service@icloud.com>
2019-05-05 11:58:37 +0900

    init

---
+++ README.md
@@ -0,0 +1,57 @@
+# collection
+
+## boost ライブラリのインストール for mac
+
+````
+brew install boost
+
+brew info boost
+//ver 1.69.0 2019-04-07
+````
+
+## 開発環境の用意
+
+### extension インストール
+extensionをインストールする。
+
+````
+code --install-extension ms-vscode.cpptools
+code --install-extension xaver.clang-format
+code --install-extension twxs.cmake
+code --install-extension vector-of-bool.cmake-tools
+code --install-extension maddouri.cmake-tools-helper
+code --install-extension donjayamanne.githistory
+````
+
+
+### extension セッティング
+
+#### clang-format
+Editor: Format On Save を有効に。
+
+#### tasks.json
+
+Command + Shift + p で、"tasks"と入力し、"Configure Tasks"を選ぶ。
+"clang++ build archive file" を選ぶと、tasks.jsonが作成される。
+
+
+#### c_cpp_property.json
+
+Command + Shift + p で、"Edit config"と入力し、"C/C++ Edit configurations"を選ぶ。
+.vscode を選ぶと、c_cpp_properties.jsonが作成される。
+
+ファイルの中の次の部分をclang に書き換える。
+
+    "compilerPath": "/usr/bin/clang++",
+
+#### cmake の設定
+
+Command + Shift + p で、"CMake"と入力し、"CMake Build"を選ぶ。
+
+そしてCLANGを選ぶ。
+
+## ビルド
+
+F7 keyでビルドができる。
+
+
---
+++ main/random.cpp
@@ -0,0 +1,74 @@
+
+#include <boost/random.hpp>
+#include <boost/random/random_device.hpp>
+#include <boost/timer/timer.hpp>
+#include <iostream>
+
+///
+///
+struct gen_array
+{
+    //コンストラクタ
+    gen_array() : count_(0), rand_engin_(seed_()) {
+        // __PRETTY_FUNCTION__ は事前定義マクロ
+        std::cout << __PRETTY_FUNCTION__ << std::endl;
+    }
+
+    // fizzbuzz言わす
+    void say() {
+        ++count_;
+
+        if ((count_ % 3 == 0) && (count_ % 5 == 0)) {
+            std::cout << "fizzbuzz" << std::endl;
+        }
+        else if (count_ % 3 == 0) {
+            std::cout << "fizz" << std::endl;
+        }
+        else if (count_ % 5 == 0) {
+            std::cout << "buzz" << std::endl;
+        }
+        else {
+            std::cout << count_ << std::endl;
+        }
+    }
+
+    std::vector<std::uint16_t> get_int_array(std::uint16_t size) {
+        std::vector<std::uint16_t> result;
+        result.reserve(size);
+
+        boost::random::uniform_int_distribution<> dist(0, 65535);  // 0から65535のサイズまでの数字
+
+        for (int i = 0; i < size; ++i) {
+            auto& n = result.emplace_back(dist(rand_engin_));
+            // std::cout << n << " loop: " << i << std::endl;
+        }
+
+        return result;
+    }
+
+    //デストラクタ
+    ~gen_array() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+
+private:
+    uint32_t count_;
+    boost::random::random_device seed_;
+    boost::random::mt19937 rand_engin_;
+};
+
+int main() {
+    boost::timer::cpu_timer timer;  // 時間計測を開始
+    timer.start();
+
+    gen_array obj;
+    uint32_t max(1000000);
+
+    auto arry = obj.get_int_array(100);
+    for (auto num : arry) {
+        std::cout << num << std::endl;
+    }
+
+    timer.stop();
+    std::cout << "===============================" << std::endl;
+    std::cout << " -- result -- " << timer.format(9, "%w s") << std::endl;
+    std::cout << "===============================" << std::endl;
+}
